c085cd4ca6b118b32de782ec3069e6ea36212c55
Alena Hrynkevich <alena@appveyor.com>
2019-08-19 21:24:59 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 # Download and instal rustup, the CLI tool form https://rustup.rs
 appveyor DownloadFile https://win.rustup.rs/ -FileName rustup-init.exe
-echo rustup-init.exe -y
+echo 'rustup-init.exe -y'
 cmd /c rustup-init.exe -y
 $env:path = $env:path + ";C:\Users\appveyor\.cargo\bin"
-echo cargo build
+echo 'cargo build'
 cmd /c cargo build
